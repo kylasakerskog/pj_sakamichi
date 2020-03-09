aac3e0ac07166aada44c51161b7cee5ea28e6556
kylasakerskog <kylasakerskog26@gmail.com>
2020-03-09 09:28:25 +0000

    finish nogizaka.py

--- masked_post.py
+++ masked_post.py
@@ -1,20 +1,31 @@
 import hinatazaka
+import nogizaka
 import requests, json
 import schedule
 import time
 
 def job():
-    msg = hinatazaka.output()
-    WEB_HOOK_URL = "WEB_HOOK_URL"
-    
+    WEB_HOOK_URL = "*****"
+    channel= "#pj_sakamichi"
+    msg = nogizaka.output(3)
     requests.post(WEB_HOOK_URL, data = json.dumps({
         'text': msg,  #通知内容
-        'username': u'hinatazaka-bot',  #ユーザー名
-        'icon_emoji': u':hinatazaka:',  #アイコン
+        'username': u'nogizaka-bot',  #ユーザー名
+        'icon_emoji': u':nogizaka-mark:',  #アイコン
         'link_names': 1,  #名前をリンク化
+        'channel': channel
+    }))
+    
+    msg = hinatazaka.output(5)
+    requests.post(WEB_HOOK_URL, data = json.dumps({
+        'text': msg,
+        'username': u'hinatazaka-bot',
+        'icon_emoji': u':hinatazaka:',
+        'link_names': 1,
+        'channel': channel
     }))
 
-schedule.every().day.at("00:00").do(job)
+schedule.every().day.at("15:07").do(job)
 
 while True:
     schedule.run_pending()
